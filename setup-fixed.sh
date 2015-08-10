6fc252275dabc674f34586c28458f954c3304379
root <root@localhost.localdomain>
2015-08-10 14:59:56 +0000

    fixing setup

--- setup-fixed.sh
+++ setup-fixed.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
+#In case your system is old: apt-get update && apt-get dist-upgrade
 git submodule init && git submodule update --recursive
-pip install --upgrade -r requirements.txt
+apt-get install -y python-dev python-setuptools libpcap0.8-dev libnetfilter-queue-dev
 apt-get install -y python-capstone python-twisted python-requests python-scapy python-dnspython python-cryptography python-crypto
 apt-get install -y python-msgpack python-configobj python-pefile python-ipy python-openssl python-pypcap
-pip install BeautifulSoup4 scapy 
-git clone https://github.com/selwin/python-user-agents
-python python-user-agents/setup.py install
-apt-get install python-dev python-setuptools libpcap0.8-dev libnetfilter-queue-dev
-pip install Pillow mitmflib
+git clone https://github.com/kti/python-netfilterqueue.git
+python python-netfilterqueue/setup.py install
+python mitmflib-0.18.4/setup.py install
